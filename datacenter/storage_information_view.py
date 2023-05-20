66b2ee16a6f78900b9640470bf91461f6e4d95a6
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-05-21 01:56:35 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,52 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
+from math import floor
 
+# Функция возвращает количество секунд от текущего момента до даты начала визита
+def get_duration(visit):
+    delta = timezone.localtime() - visit.entered_at
+    seconds = delta.total_seconds()
+    return seconds 
+
+# Фунция переводит секунды в часы и минуты
+def format_duration(duration):
+    hours = floor(duration // 3600)
+    mins = floor((duration-hours*3600) // 60)
+    return print(str(hours)+'ч '+str(mins)+'мин')
+
+# Функция переводит секунды в часы, минуты и секунды
+def format_time(seconds):
+    hours = floor(seconds // 3600)
+    mins = floor((seconds-hours*3600) // 60)
+    secs = floor(seconds - hours * 3600 - mins * 60)
+    return '{}:{:02}:{:02}'.format(hours, mins, secs)
 
 def storage_information_view(request):
     # Программируем здесь
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    # Задание 8. Получить активные визиты пользователей
+    myList = []
+    visit_in = Visit.objects.filter(leaved_at = None)
+    for visit in visit_in:
+        seconds = get_duration(visit)  
+        time_inside = format_time(seconds)
+        passcard = visit.passcard
+        pas_name = str(passcard.owner_name)
+        time_in = visit.entered_at
+        myDict = {
+            "who_entered": pas_name,
+            "entered_at": time_in,
+            'duration': time_inside
+            }
+        myList.append(myDict)
+
+    non_closed_visits = myList
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
     return render(request, 'storage_information.html', context)
+    # Задание 8. Конец
+
+
