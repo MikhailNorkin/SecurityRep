13d2d3b12ef4133fc052b5167ee422d73dbeb3a8
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-11 01:34:27 +0300

    Add readme

--- project/settings.py
+++ project/settings.py
@@ -1,26 +1,26 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5'
+        'ENGINE': os.getenv("DATABASES_ENGINE"),
+        'HOST': os.getenv("DATABASES_HOST"),
+        'PORT': os.getenv("DATABASES_PORT"),
+        'NAME': os.getenv("DATABASES_NAME") ,
+        'USER': os.getenv("DATABASES_USER"),
+        'PASSWORD': os.getenv("DATABASES_PASSWORD")
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
-
-DEBUG = True
-
 ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
@@ -30,7 +30,6 @@ TEMPLATES = [
         'APP_DIRS': True,
     },
 ]
-
 
 USE_L10N = True
 
